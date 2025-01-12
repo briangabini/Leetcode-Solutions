82e045a29dea644014d4684e17dcf1310ade7acf
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2025-01-12 17:53:31 +0800

    feat: add solution for min stack

---
+++ src/com/bgcoding/java/stack/p155/minstack/MinStack.java
@@ -0,0 +1,47 @@
+package com.bgcoding.java.stack.p155.minstack;
+
+import java.util.Stack;
+
+// Optimal Solution
+// Time: O(1)
+// Space: O(n)
+class MinStack {
+
+    private Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (!stack.empty()) {
+            // check previous min
+            int previousMin = stack.peek()[1];
+            int newMin = Math.min(val, previousMin);
+            stack.push(new int[]{val, newMin});
+        } else {
+            stack.push(new int[]{val, val});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
